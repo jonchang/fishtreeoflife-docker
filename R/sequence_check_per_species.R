b4b1a56ede5647025c372f76af58e6bde2f3a4cd
Striker528 <austyn.j.adams@gmail.com>
2022-03-30 08:26:13 -0700

    Fixed the issues that were outlines and cleaned up the comments

--- R/sequence_check_per_species.R
+++ R/sequence_check_per_species.R
@@ -6,29 +6,10 @@ sequence_check_per_species <- function(partition_file, DNA_file){
   
   labridae_phylip.dna <- ape::read.dna(xzfile(DNA_file), format = "sequential", as.matrix = TRUE)
   
-  path <- getwd()
-  full_name <- paste(path, partition_file, sep = "")
-  partition_df <- parsing_partitions(full_name)
+  partition_df <- parsing_partitions(partition_file)
   
   vector_species <- vectorized_search_per_species(partition_df, labridae_phylip.dna)
-  #print(vector_species)
   
-  #Now any gene that was not in a species will have a mean of 0
-  means_of_genes <- colMeans(vector_species)
-  
-  len_means <- length(means_of_genes)
-  
-  for (i in 1:len_means){
-    if (means_of_genes[i] == 0){
-      print("This gene was not sequenced:")
-      print(partition_df[i, 1])
-    }
-  }
-  
+  return(vector_species)
   
 }
-
-#Testing
-#fileName <- '/final_alignment.partitions'
-#DNA_file <- "Labridae.phylip.xz"
-#sequence_check_per_species(fileName, DNA_file)
--- R/vectorized_search_per_species.R
+++ R/vectorized_search_per_species.R
@@ -1,22 +1,22 @@
 vectorized_search_per_species <- function(parsed_partition, full_genes){
   gene_names <- parsed_partition$gene_Name
   #First create a matrix to store if a species has a gene
-  mat1 <- matrix(0,nrow=nrow(full_genes),ncol=length(gene_names),byrow=TRUE)
-  colnames(mat1) <- gene_names
-  rownames(mat1) <- rownames(full_genes)
+  mat1 <- matrix(0, 
+                 nrow=nrow(full_genes), 
+                 ncol=length(gene_names), 
+                 byrow=TRUE,
+                 dimnames = list(rownames(full_genes), gene_names)
+                )
   
-  length_gene_places = nrow(parsed_partition)
-  
-  row_number <- nrow(full_genes)
+  #This is the value for when there are no A's, C's, T's, or G's
+  gap_character <- as.raw(4)
   
   #Need to go through each species
-  for(row in 1:row_number) {
+  for(row in 1:nrow(full_genes)){
     #Go through all the sections from the partition file
-    for(i in 1:length_gene_places){
+    for(i in 1:nrow(parsed_partition)){
       #Go through all the sections from the partition file
-      #page 35 of the book
-      #a or c or t or g : 11110000 = 240
-      if(all(full_genes[ row, strtoi(parsed_partition[i, 2]) : strtoi(parsed_partition[i, 3]) ] == as.raw(4)) == FALSE){
+      if(all(full_genes[ row, parsed_partition[i, 2] : parsed_partition[i, 3] ] == gap_character) == FALSE){
         #There was some nucletoide in the range
         mat1[row, i] <- 1
       }
