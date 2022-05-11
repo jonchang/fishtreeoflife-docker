adac54986ca8c236cc003df78aa89d0150e644c8
Striker528 <austyn.j.adams@gmail.com>
2022-05-11 09:01:42 -0700

    Fixed the files

--- R/generate_taxonomy.R
+++ R/generate_taxonomy.R
@@ -31,6 +31,10 @@ dna %<-% scan("downloads/final_alignment.phylip.xz", what = list(character(), ch
 charsets <- readLines("downloads/final_alignment.partitions") %>% str_replace_all(fixed("DNA, "), "")
 tiprates <- read_csv("downloads/tiprates.csv.xz")
 
+
+partition_file <- "downloads/final_alignment.phylip.xz"
+DNA_file <- "downloads/actinopt_full.trees.xz"
+
 ## Correct tiprates names
 taxconvert <- read_csv("downloads/taxonConversion.csv") %>% mutate_all(str_replace_all, "_", " ") %>% filter(!is.na(fishbase))
 
@@ -61,7 +65,7 @@ two_col_to_list <- function(df) {
 }
 
 generate_rank_data <- function(df, current_rank, downloadpath) {
-    list_of_genes_and_species <- apply_df_vectors_genes_and_their_species(full_name, DNA_file)
+    list_of_genes_and_species <- apply_df_vectors_genes_and_their_species(partition_file, DNA_file)
     total_list_genes_and_their_species <- list(sampled_loci = list_of_genes_and_species)
     
     out <- list()
